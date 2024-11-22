64e671debc73999d6006ae4587a129785b14e7f6
Wander Amaral <wanderamaral013@gmail.com>
2024-11-22 10:32:37 -0300

    commit

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,38 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> list_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte super luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 50.5
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte luxo",
+                ValorDiariaAdulto = 100,
+                ValorDiariaCrianca = 40.5
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte single",
+                ValorDiariaAdulto = 90,
+                ValorDiariaCrianca = 30.5
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte crise",
+                ValorDiariaAdulto = 80,
+                ValorDiariaCrianca = 20.5
+            }
+        };
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,8 +2,41 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.list_quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkout.MinimumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+		dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker elemento = sender as DatePicker;
+
+		DateTime data_selecionada_checkin = elemento.Date;
+
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddMonths(6);
+    }
 }
